90755d3cd98a6f7b28e09134cdd1f11c8ff1bb42
wntitus <wesn.titus@gmail.com>
2018-07-30 19:29:37 -0400

    adding firebase script tag and intializiation

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBmQ7bMGC-2b5caZ5S780Z1ATp3oVm_0H0",
+    authDomain: "rps-multiplayer-9b5ab.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-9b5ab.firebaseio.com",
+    projectId: "rps-multiplayer-9b5ab",
+    storageBucket: "rps-multiplayer-9b5ab.appspot.com",
+    messagingSenderId: "1044802644044"
+  };
+  firebase.initializeApp(config);
